b90ed59439a22f43d02edf5950cf67fbd9b840cc
tanvirch0750 <tanvir0750@yahoo.com>
2023-03-29 23:30:33 +0600

    implemented home page markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,77 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { Inter } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        {/* NAVBAR */}
+        <nav className="bg-white p-2 flex justify-between items-center text-[#0f1f47]">
+          <a href="" className="font-bold text-gray-700 text-2xl">
+            BookTable
           </a>
-        </div>
-      </div>
+          <div>
+            <div className="flex">
+              <button className="bg-blue-400 text-white border p-2 px-4 rounded mr-3 text-lg">
+                Signin
+              </button>
+              <button className="border p-2 rounded px-4 text-lg">
+                Signup
+              </button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+        <main>
+          {/* HEADER */}
+          <header className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+            <div className="text-center mt-10">
+              <h1 className="text-white text-5xl font-bold mb-2">
+                Find your table for any occasion
+              </h1>
+              <div className="text-left py-3 m-auto flex justify-center items-center">
+                <input
+                  type="text"
+                  placeholder="State, city or town"
+                  className="rounded text-lg mr-3 p-2 w-[450px] mt-4"
+                />
+                <button className="bg-red-600 px-9 text-white py-3 mt-4 rounded">
+                  Let's go
+                </button>
+              </div>
+            </div>
+          </header>
+          {/* HEADER */}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          {/* CARDS */}
+          <section className="py-3 px-36 mt-10 flex flex-wrap">
+            {/* card */}
+            <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer ">
+              <img
+                src="https://resizer.otstatic.com/v2/photos/wide-medium/2/51464796.jpg"
+                alt="food image"
+                className="w-full h-36"
+              />
+              <div className="p-1">
+                <h3 className="font-bold text-2xl mb-2">Milestoned Grill</h3>
+                <div className="flex items-start">
+                  <div className="flex mb-2 tracking-wide text-lg">*****</div>
+                  <p className="ml-2 font-semibold">77 reviews </p>
+                </div>
+                <div className="flex font-light capitalize">
+                  <p className="text-reg mr-3">Mexican</p>
+                  <p className="mr-2">$500</p>
+                  <p>Torornto</p>
+                </div>
+                <p className="text-sm mt-1 fonst-bold">Booked 3 times today</p>
+              </div>
+            </div>
+            {/* card */}
+          </section>
+          {/* CARDS */}
+        </main>
+      </main>
     </main>
-  )
+  );
 }
